3d9992cd81828ef404027680ed421b801a042811
Sergey <karandeev73@gmail.com>
2021-03-09 20:45:08 +0300

    feat: add form courses

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const homeRoutes = require('./routes/home');
+const coursesRoutes = require('./routes/courses')
+const addRoutes = require('./routes/add')
 
 const app = express();
 
@@ -14,30 +17,14 @@ app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}))
+
+app.use('/',homeRoutes);
+app.use('/courses',coursesRoutes);
+app.use('/add', addRoutes);
 
 
-app.get('/', (req, res) =>{
-    res.render('index', {
-        title: 'Главная страница',
-        isHome: true
-    })
-})
 
-app.get('/courses', (req, res) => {
-    res.render('courses', {
-        title: 'Курсы',
-        isCourses: true
-    })
-
-app.get('/add', (req, res) => {
-    res.render('add', {
-        title: 'Добавить курсы',
-        isAdd: true
-    })
-})
-
-
-})
 
 const PORT = process.env.PORT || 3000;
 
---
+++ routes/add.js
@@ -0,0 +1,17 @@
+const {Router} = require('express');
+const router = Router();
+
+router.get('/', (req, res) => {
+    res.render('add', {
+        title: 'Добавить курсы',
+        isAdd: true
+    })
+})
+
+router.post('/', (req, res) => {
+    console.log(req.body)
+
+    res.redirect('/courses')
+})
+
+module.exports = router;
